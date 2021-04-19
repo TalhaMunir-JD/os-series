428a821d50bf92d4f9c4a70f9c337911891d14d4
TalhaMunir-JD <61348658+TalhaMunir-JD@users.noreply.github.com>
2021-04-19 13:36:14 +0500

    Update main.c

--- os-series/src/impl/kernel/main.c
+++ os-series/src/impl/kernel/main.c
@@ -1,12 +1,24 @@
 #include "print.h"
 void kernel_main(){
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("ssssssssss  eeeeeeeee  eeeeeeeee  cccccccccccc  ssssssssss \n");
-    print_str("s           e          e          c             s \n");
-    print_str("s           e          e          c             s \n");
-    print_str("ssssssssss  eeeeeeeee  eeeeeeeee  c             ssssssssss \n");
-    print_str("         s  e          e          c                      s \n");
-    print_str("         s  e          e          c                      s \n");
-    print_str("ssssssssss  eeeeeeeee  eeeeeeeee  cccccccccccc  ssssssssss \n");
+    print_set_color(PRINT_COLOR_RED, PRINT_COLOR_BLACK);
+                                                                              
+    print_str("      *******         ***** **         ***** **          * ***          *******  \n");
+    print_str("    *       ***    ******  **** *   ******  **** *     *  ****  *     *       ***\n");
+    print_str("   *         **   **   *  * ****   **   *  * ****     *  *  ****     *         **\n");
+    print_str("   **        *   *    *  *   **   *    *  *   **     *  **   **      **        * \n");
+    print_str("    ***              *  *             *  *          *  ***            ***        \n");
+    print_str("   ** ***           ** **            ** **         **   **           ** ***      \n");
+    print_str("    *** ***         ** **            ** **         **   **            *** ***    \n");
+    print_str("      *** ***       ** ******        ** ******     **   **              *** ***  \n");
+    print_str("        *** ***     ** *****         ** *****      **   **                *** ***\n");
+    print_str("          ** ***    ** **            ** **         **   **                  ** **\n");
+    print_str("           ** **    *  **            *  **          **  **                   ** *\n");
+    print_str("            * *        *                *            ** *      *              * *\n");
+    print_str("  ***        *     ****         *   ****         *    ***     *     ***        * \n");
+    print_str(" *  *********     *  ***********   *  ***********      *******     *  *********  \n");
+    print_str("*     *****      *     ******     *     ******           ***      *     *****    \n");
+    print_str("*                *                *                               *              \n");
+    print_str(" **               **               **                              **            \n");                                     
+                                                  
 }
